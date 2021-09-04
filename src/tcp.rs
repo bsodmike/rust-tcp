7b195dfb4ea448f9367a630bd9daf4716c8b916c
Michael de Silva <michael@mwdesilva.com>
2021-09-04 15:57:43 +0530

    Initial handling of SYN,ACK

--- src/tcp.rs
+++ src/tcp.rs
@@ -2,15 +2,17 @@
 use std::io;
 use std::io::prelude::*;
 
-enum State {
+pub enum State {
     Closed,
     Listen,
-    // SynRcvd,
+    SynRcvd,
     // Estab,
 }
 
 pub struct Connection {
     state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
 }
 
 /// State of the Send Sequence Space (RFC 793 S3.2 F4)
@@ -27,24 +29,24 @@ pub struct Connection {
 ///   3 - sequence numbers allowed for new data transmission
 ///   4 - future sequence numbers which are not yet allowed
 /// ```
-pub struct SendSequenceSpace {
+struct SendSequenceSpace {
     /// send unacknowledged
-    una: usize,
+    una: u32,
     /// send next
-    nxt: usize,
+    nxt: u32,
     /// send window
-    wnd: usize,
+    wnd: u16,
     /// send urgent pointer
     up: bool,
     /// segment sequence number used for last window update
-    wl1: usize,
+    wl1: u32,
     /// segment acknowledgement number used for last window update
-    wl2: usize,
+    wl2: u32,
     /// initial send sequence number
-    iss: usize,
+    iss: u32,
 }
 
-/// Receive Sequence Space (RFC 793 S3.2 F5)
+/// State of the Receive Sequence Space (RFC 793 S3.2 F5)
 ///
 /// ```
 ///                  1          2          3
@@ -58,81 +60,109 @@ pub struct SendSequenceSpace {
 /// ```
 struct RecvSequenceSpace {
     /// send next
-    nxt: usize,
+    nxt: u32,
     /// send window
-    wnd: usize,
+    wnd: u16,
     /// send urgent pointer
     up: bool,
     /// initial receive sequence number
-    irs: usize,
+    irs: u32,
 }
 
-impl Default for Connection {
+impl Default for State {
     fn default() -> Self {
-        // State::Closed
-        Connection {
-            state: State::Listen,
-        }
+        State::Listen
     }
 }
 
-impl State {
+impl Connection {
+    pub fn accept<'a>(
+        nic: &mut tun_tap::Iface,
+        iph: etherparse::Ipv4HeaderSlice<'a>,
+        tcph: etherparse::TcpHeaderSlice<'a>,
+        data: &'a [u8],
+    ) -> io::Result<Option<Self>> {
+        let mut buf = [0u8; 1500];
+        if !tcph.syn() {
+            // only expected SYN packet
+            return Ok(None);
+        }
+
+        let iss = 0;
+        let mut c = Connection {
+            state: State::SynRcvd,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss + 1,
+                wnd: 10,
+                up: false,
+                wl1: 0,
+                wl2: 0,
+            },
+            recv: RecvSequenceSpace {
+                irs: tcph.sequence_number(),
+                nxt: tcph.sequence_number() + 1,
+                wnd: tcph.window_size(),
+                up: false,
+            },
+        };
+
+        // need to start establishing a connection
+        let mut syn_ack = etherparse::TcpHeader::new(
+            tcph.destination_port(),
+            tcph.source_port(),
+            c.send.iss,
+            c.send.wnd,
+        );
+
+        syn_ack.acknowledgment_number = c.recv.nxt;
+        syn_ack.syn = true;
+        syn_ack.ack = true;
+        let mut ip = etherparse::Ipv4Header::new(
+            syn_ack.header_len(),
+            64,
+            etherparse::IpTrafficClass::Tcp,
+            [
+                iph.destination()[0],
+                iph.destination()[1],
+                iph.destination()[2],
+                iph.destination()[3],
+            ],
+            [
+                iph.source()[0],
+                iph.source()[1],
+                iph.source()[2],
+                iph.source()[3],
+            ],
+        );
+
+        syn_ack.checksum = syn_ack
+            .calc_checksum_ipv4(&ip, &[])
+            .expect("failed to compute checksum");
+        eprintln!("got ip header:\n{:02x?}", iph);
+        eprintln!("got tcp header:\n{:02x?}", tcph);
+
+        // write out the headers
+        let unwritten = {
+            let mut unwritten = &mut buf[..];
+            ip.write(&mut unwritten);
+            syn_ack.write(&mut unwritten);
+            unwritten.len()
+        };
+
+        eprintln!("responding with {:02x?}", &buf[..buf.len() - unwritten]);
+        nic.send(&buf[..unwritten])?;
+        Ok(Some(c))
+    }
+
     pub fn on_packet<'a>(
         &mut self,
         nic: &mut tun_tap::Iface,
         iph: etherparse::Ipv4HeaderSlice<'a>,
         tcph: etherparse::TcpHeaderSlice<'a>,
         data: &'a [u8],
-    ) -> io::Result<usize> {
-        let mut buf = [0u8; 1500];
-        match *self {
-            State::Closed => {
-                return Ok(0);
-            }
-            State::Listen => {
-                if !tcph.syn() {
-                    // only expected SYN packet
-                    return Ok(0);
-                }
-
-                // need to start establishing a connection
-                let mut syn_ack = etherparse::TcpHeader::new(
-                    tcph.destination_port(),
-                    tcph.source_port(),
-                    unimplemented!(),
-                    unimplemented!(),
-                );
-                syn_ack.syn = true;
-                syn_ack.ack = true;
-                let mut ip = etherparse::Ipv4Header::new(
-                    syn_ack.header_len(),
-                    64,
-                    etherparse::IpTrafficClass::Tcp,
-                    [
-                        iph.destination()[0],
-                        iph.destination()[1],
-                        iph.destination()[2],
-                        iph.destination()[3],
-                    ],
-                    [
-                        iph.source()[0],
-                        iph.source()[1],
-                        iph.source()[2],
-                        iph.source()[3],
-                    ],
-                );
-
-                // write out the headers
-                let unwritten = {
-                    let mut unwritten = &mut buf[..];
-                    ip.write(&mut unwritten);
-                    syn_ack.write(&mut unwritten);
-                    unwritten.len()
-                };
-
-                nic.send(&buf[..unwritten]);
-            }
-        }
-        // Ok(())
+    ) -> io::Result<()> {
+        Ok(())
     }
 }
